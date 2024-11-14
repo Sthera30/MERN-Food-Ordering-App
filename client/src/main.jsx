b9cc6b084e7c88f0066e31fa71a67b4c395e2dff
Anelisa Maneli <Anelisamaneli@Katlegos-MacBook-Air-2.local>
2024-11-15 00:06:14 +0200

    Updated mainjsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import './css/ExploreMore.css'
 import { UserContextProvider } from "./context/userContext.jsx";
 import { FoodContextProvider } from "./context/foodContext.jsx";
 import { CartContextProvider } from "./context/cartContext.jsx";
