e4ef77f50257fc89674c41b25ef7a9ce98f500e7
Anelisa Maneli <Anelisamaneli@Katlegos-MacBook-Air-2.local>
2024-11-14 21:25:12 +0200

    URL updates

--- server/index.js
+++ server/index.js
@@ -17,8 +17,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 const allowedOrigins = [
-   // 'http://localhost:5173', // Local frontend URLrrrrr
-    'https://mern-food-ordering-backend-app.onrender.com',  // for production
+    'http://localhost:5173', // Local frontend URLrrrrr
+     // for production
     'https://mern-food-ordering-frontend-app-3502.onrender.com'
   ];
   
@@ -33,6 +33,17 @@ const allowedOrigins = [
           }
       }
   }));
+
+  app.options('*', cors()); // Allow preflight requests for all routes
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', allowedOrigins.includes(req.headers.origin) ? req.headers.origin : '');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
   
   
 
